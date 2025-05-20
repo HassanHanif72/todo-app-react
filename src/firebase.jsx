6a26ecde7b507084a9d3b67fef00a44f59fa1b91
hassanhanif72 <hassanhanif72@gmail.com>
2025-05-21 02:12:30 +0500

    upload

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDncJ-rPMYpaATDiOpP1zj_E2VmtXU9U30",
-    authDomain: "todo-app-react-e24d1.firebaseapp.com",
-    projectId: "todo-app-react-e24d1",
-    storageBucket: "todo-app-react-e24d1.firebasestorage.app",
-    messagingSenderId: "719169499619",
-    appId: "1:719169499619:web:ce35ca79a1a377838230d6"
+    // config code
 };
 
 const initApp = initializeApp(firebaseConfig);
